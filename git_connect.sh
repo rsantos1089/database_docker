1cdf7fb930f7ef5aa87593730d387dab5fa10ef1
rsantos1089 <45879453+rsantos1089@users.noreply.github.com>
2025-05-14 17:18:32 -0500

    Update git_connect.sh

--- git_connect.sh
+++ git_connect.sh
@@ -1,5 +1,11 @@
-#create ssh
-ssh-keygen -t rsa -C "rudye@iadb.org" -f .ssh/iadb
+## WINDOWS
+ssh-keygen -t rsa -b 4096 -C "rudye@corp.org" -f .ssh/corp
 eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/iadb
+ssh-add ~/.ssh/corp
+
+## MAC
+#create ssh
+ssh-keygen -t rsa -C "rudye@corp.org" -f .ssh/corp
+eval "$(ssh-agent -s)"
+ssh-add -K ~/.ssh/corp
 
